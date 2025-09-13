125fd445e08eebe70d88f3be8b283e5406a8c21d
Omprakash770 <omprakash.d@pionglobal.com>
2025-09-13 12:39:08 +0530

    Initial commit - Tvara Backend Challenge

--- src/interview-tests/dto/create-interview-test.dto.ts
+++ src/interview-tests/dto/create-interview-test.dto.ts
@@ -1,7 +1,10 @@
 import { Type } from 'class-transformer';
-import { IsObject, IsNumber, IsBoolean } from 'class-validator';
+import { IsObject, IsNumber, IsBoolean, IsString } from 'class-validator';
 
 export class CreateInterviewTestDto {
+  @IsString()
+  name: string;
+
   @IsObject()
   field_1: Record<string, any>; // <-- allows JSON object directly
 
